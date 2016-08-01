7d5629b12115ea6373200b16ed2c7ac395771154
Mattias Runge <mattias@runge.se>
2016-08-01 20:11:25 +0200

    Added face detection API via opencv
    Updated all npm deps

---
+++ test/face.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const getPort = require("get-port");
+const chai = require("chai");
+const api = require("api.io").client;
+const main = require("../lib/main");
+const configuration = require("../lib/configuration");
+
+chai.use(require("chai-datetime"));
+require("../lib/test");
+const assert = chai.assert;
+
+describe("Face", function() {
+    this.timeout(20000);
+
+    before(function*() {
+        let args = {
+            level: "debug",
+            config: "test/data/config.json",
+            port: yield getPort(),
+            keys: [ "secret key" ]
+        };
+
+        yield main.start(args);
+    });
+
+    after(function*() {
+        yield main.stop();
+    });
+
+    describe("Setup", () => {
+        it("should connect", function*() {
+            yield api.connect({
+                hostname: "localhost",
+                port: configuration.port
+            });
+        });
+
+        it("should authenticate", function*() {
+            let result = yield api.auth.identify("secret key");
+
+            assert.ok(result);
+        });
+    });
+
+    describe("Detect", () => {
+        it("should detect faces", function*() {
+            let list = yield api.face.detect("node_modules/opencv/examples/files/mona.png");
+
+            assert.equal(list.length, 1);
+            assert.equal(list[0].x, 225);
+            assert.equal(list[0].y, 187);
+            assert.equal(list[0].w, 210);
+            assert.equal(list[0].h, 210);
+        });
+
+    });
+});
